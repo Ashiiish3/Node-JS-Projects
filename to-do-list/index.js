9db9cce0671dd591586a9c69f66423842e75ad97
Ashiiish3 <Ashiiish777@gmail.com>
2024-10-14 22:04:26 +0530

    update

--- to-do-list/index.js
+++ to-do-list/index.js
@@ -36,6 +36,30 @@ app.post('/addProduct', (req, res)=>{
     })
 })
 
+app.put('/updateProduct/:id', (req, res)=>{
+    const {id} = req.params
+    fs.readFile('./db.json', 'utf-8', (err, data)=>{
+        if(err){
+            console.log(err)
+        }else{
+            const newData = JSON.parse(data)
+            const index = newData.products.findIndex((ele)=> ele.id == id)
+            if(index != -1){
+                newData.products[index] = {...newData.products[index], ...req.body}
+                fs.writeFile('./db.json', JSON.stringify(newData), (err)=>{
+                    if(err){
+                        console.log(err)
+                    }else{
+                        res.send("data updated.")
+                    }
+                })
+            }else{
+                res.send("Product not found.")
+            }
+        }
+    })
+})
+
 app.delete('/deleteProduct/:id', (req, res)=>{
     const {id} = req.params
     fs.readFile('./db.json', "utf-8", (err, data)=>{
