d4763ec285a6afbf2cea742c9dca024c011ef6b4
Ashiiish3 <Ashiiish777@gmail.com>
2024-12-17 16:12:55 +0530

    update

--- fullstack-project-crud/server/controllers/notes.controller.js
+++ fullstack-project-crud/server/controllers/notes.controller.js
@@ -1,5 +1,53 @@
-const notesCreate = (req, res) => {
-    const { title, content, userId } = req.body
+const notesModel = require("../models/notes.model")
+// Creating notes
+const notesCreate = async (req, res) => {
+    const { title, content } = req.body
+    if (!title || !content) {
+        return res.status(400).send({ message: "Please fill in all fields." })
+    }
+    try {
+        await notesModel.create({ title, content, userId: req.user._id })
+        res.status(200).json({ message: "Note has been created successfully." })
+    } catch (error) {
+        res.status(400).json({ message: error })
+    }
 }
+// Deleting notes
+const notesDelete = async (req, res) => {
+    const { notesId } = req.params
+    const isNoteExist = await notesModel.findById(notesId)
+    try {
+        if (!isNoteExist) {
+            return res.status(400).json({ message: "Notes not Exist." })
+        }
+        if (isNoteExist.userId != req.user._id) {
+            return res.status(400).json({ message: "You don't have permission to detete this Note." })
+        }
+        await notesModel.findByIdAndDelete(notesId)
+        res.status(200).json({ message: "Note has been deleted." })
+    } catch (error) {
+        res.status(400).json({ message: error })
+    }
+}
+// Gets all notes of user
+const getAllNotes = async (req, res) => {
+    const { userId } = req.params
+    try {
+        if (userId != req.user._id) {
+            return res.status(403).json({ message: "You don't have permission to view this note." })
+        }
+        const allUserNotes = await notesModel.find({ userId })
+        if (allUserNotes.length == 0) {
+            return res.status(400).json({ message: "No notes found." })
+        }
+        res.status(200).json({ allUserNotes })
+    } catch (error) {
+        res.status(400).json({ message: error })
+    }
+}
+// Gets notes by id of user
+const getSingleNote = async (req, res) => {
+    const { noteId } = req.params
 
-module.exports = { notesCreate }
+}
+module.exports = { notesCreate, notesDelete, getAllNotes, getSingleNote }
--- fullstack-project-crud/server/routes/notes.routes.js
+++ fullstack-project-crud/server/routes/notes.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { notesCreate } = require("../controllers/notes.controller");
+const { notesCreate, notesDelete, getSingleNote, getAllNotes } = require("../controllers/notes.controller");
 const isAuth = require("../middleware/Auth");
 
 const notesRouter = express.Router()
 
 notesRouter.post("/create", isAuth, notesCreate)
-
+notesRouter.delete("/delete/:notesId", isAuth, notesDelete)
+notesRouter.get("/getAllNotes/:userId", isAuth, getAllNotes)
+notesRouter.get("/getSingleNote/:noteId", isAuth, getSingleNote)
 module.exports = notesRouter;
