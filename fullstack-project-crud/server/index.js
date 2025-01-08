afc08dad8ace8a52a7f22c0a00c8db5eade237c1
Ashiiish3 <Ashiiish777@gmail.com>
2025-01-08 15:43:08 +0530

    update

--- fullstack-project-crud/server/index.js
+++ fullstack-project-crud/server/index.js
@@ -11,7 +11,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.static("./UploadsImages"));
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:4000', "http://localhost:5000", "http://localhost:6000"],
+    origin: ['http://localhost:3000', 'http://localhost:4000', "http://localhost:5000", "http://localhost:6000", "https://fullstack-cred-project-server.onrender.com"],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
 }))
