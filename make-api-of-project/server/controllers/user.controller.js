a69b33111403804ee34eaaa02a98899c1cf4cbea
Ashiiish3 <Ashiiish777@gmail.com>
2025-01-11 17:26:59 +0530

    update

--- make-api-of-project/server/controllers/user.controller.js
+++ make-api-of-project/server/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const userModel = require("../models/user.model")
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 const userSignUp = async (req, res) => {
     const { userName, email, dateOfBirth, role, location, password, confirmPassword } = req.body
@@ -28,25 +30,33 @@ const userSignUp = async (req, res) => {
 const userSignIn = async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
-        return res.status().json({ message: "Please fill All fields." })
+        return res.status(400).json({ message: "Please fill All fields." })
     }
     try {
         const isUserExist = await userModel.findOne({ email })
         if (!isUserExist) {
-            return res.status().json({ message: "Please sign up first." })
+            return res.status(400).json({ message: "Please sign up first." })
         }
         bcrypt.compare(password, isUserExist.password, async (err, result) => {
             if (err) {
                 res.status(500).json({ message: "Error is comparing Password." })
             }
             if (result) {
-                res.status(200).json({ message: "Login Successfully.", userData: isUserExist })
+                const { password, ...rest } = isUserExist._doc
+                console.log(rest)
+                jwt.sign({ userData: rest }, process.env.PRIVATEKEY, { expiresIn: "1h" }, (err, token) => {
+                    if (err) {
+                        return res.status(400).json({ message: "Error in creating token" })
+                    }
+                    if (token) {
+                        res.cookie("AccessToken", token).status(200).json({ message: "Login Successfully.", userData: rest })
+                    }
+                })
             }
         })
     } catch (error) {
         res.status(400).send({ message: "Error creating Sing Ip", error })
     }
-
 }
 
 module.exports = { userSignUp, userSignIn }
--- make-api-of-project/server/index.js
+++ make-api-of-project/server/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const connectWithDb = require("./config/db")
 const userRoutes = require("./routes/user.routes")
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/user", userRoutes)
 
