c926be345d2cd131eed776eb44bb8641f9ac08b3
Ashiiish3 <Ashiiish777@gmail.com>
2025-01-31 21:46:08 +0530

    update

--- movie-projects/client/src/pages/Register.jsx
+++ movie-projects/client/src/pages/Register.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 export default function Register() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const postRegisterInfo = (e) => {
+    e.preventDefault()
+    try {
+      // const res = axios.post("http://localhost:4000/auth/register", {name, email, password})
+      // console.log(res)
+      toast.success("OTP has been sent to your email. Please check your gmail.", { theme: "dark",autoClose: 3000 })
+    } catch (error) {
+      console.log(error)
+      toast.error("Failed to send OTP and try again.", { theme: "dark", autoClose: 3000 })
+    }
+  }
+
   return (
     <div className="h-[85vh] flex justify-center items-center bg-gray-800">
       <div className="w-[400px] bg-gray-900 shadow-lg rounded-lg p-6">
         <h1 className="text-3xl font-semibold text-center text-white mb-4">Register</h1>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={(e) => postRegisterInfo(e)}>
           <div className="flex flex-col">
             <label className="font-medium mb-1 text-gray-300">Enter Your Name</label>
             <input
               type="text"
               placeholder="John Doe"
               className="border border-gray-600 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]"
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="flex flex-col">
@@ -21,6 +39,7 @@ export default function Register() {
               type="email"
               placeholder="example@gmail.com"
               className="border border-gray-600 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="flex flex-col">
@@ -29,6 +48,7 @@ export default function Register() {
               type="password"
               placeholder="********"
               className="border border-gray-600 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button
--- movie-projects/server/index.js
+++ movie-projects/server/index.js
@@ -3,6 +3,7 @@ const { connectWithDB } = require("./utils/db")
 const userRoutes = require("./routes/user.routes")
 const cookieParser = require('cookie-parser')
 const movieRoutes = require("./routes/movie.routes")
+const cors = require("cors")
 require('dotenv').config()
 
 const app = express()
@@ -10,15 +11,21 @@ app.set("view engine", "ejs")
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.static("./uploads"));
+app.use(cors({
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:4000", "http://localhost:5000"],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+}))
 
 app.use("/auth", userRoutes)
 app.use("/movie", movieRoutes)
 
-app.listen(process.env.PORT || 5000, async ()=>{
+app.listen(process.env.PORT || 5000, async () => {
     try {
         await connectWithDB
         console.log(`server is running on port ${process.env.PORT}`)
     } catch (error) {
-        console.log("Error Create server",error)
+        console.log("Error Create server", error)
     }
 })
