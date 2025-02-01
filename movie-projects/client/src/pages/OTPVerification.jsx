f11754114fc89916eeefa1c599162d67a82d36e9
Ashiiish3 <Ashiiish777@gmail.com>
2025-02-01 21:37:29 +0530

    update

--- movie-projects/client/src/pages/OTPVerification.jsx
+++ movie-projects/client/src/pages/OTPVerification.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function OTPVerification() {
+    const [otp, setOtp] = useState("")
+    console.log(otp)
+    const InputChange = (e) => {
+        console.log(e.target.value)
+    }
+    const postOTP = ()=> {
+
+    }
     return (
-        <div class="flex items-center justify-center h-[85vh]">
-            <div class="bg-gray-900 p-6 rounded-lg shadow-lg max-w-sm w-full text-center">
-                <h2 class="text-2xl font-semibold text-white mb-4">OTP Verification</h2>
-                <p class="text-gray-400 mb-4">We have sent a verification code to your email. Please enter the OTP below.</p>
-                <div class="flex justify-center gap-2 mb-4">
-                    <input type="text" maxlength="1" class="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
-                    <input type="text" maxlength="1" class="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
-                    <input type="text" maxlength="1" class="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
-                    <input type="text" maxlength="1" class="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
-                    <input type="text" maxlength="1" class="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
-                    <input type="text" maxlength="1" class="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
+        <div className="flex items-center justify-center h-[85vh]">
+            <div className="bg-gray-900 p-6 rounded-lg shadow-lg max-w-sm w-full text-center">
+                <h2 className="text-2xl font-semibold text-white mb-4">OTP Verification</h2>
+                <p className="text-gray-400 mb-4">We have sent a verification code to your email. Please enter the OTP below.</p>
+                <div className="flex justify-center gap-2 mb-4">
+                    <input type="text" maxLength="1" onChange={(e)=>InputChange(e)} className="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
+                    <input type="text" maxLength="1" onChange={(e)=>InputChange(e)} className="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
+                    <input type="text" maxLength="1" onChange={(e)=>InputChange(e)} className="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
+                    <input type="text" maxLength="1" onChange={(e)=>InputChange(e)} className="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
+                    <input type="text" maxLength="1" onChange={(e)=>InputChange(e)} className="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
+                    <input type="text" maxLength="1" onChange={(e)=>InputChange(e)} className="w-12 h-12 text-center text-white bg-gray-700 border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]" />
                 </div>
-                <button class="w-full bg-[#ACFC03] hover:bg-[#adfc03e1] font-semibold py-2 rounded-lg transition text-black mt-3">
+                <button className="w-full bg-[#ACFC03] hover:bg-[#adfc03e1] font-semibold py-2 rounded-lg transition text-black mt-3">
                     Verify OTP
                 </button>
-                <p class="text-gray-400 text-sm mt-2">
+                <p className="text-gray-400 text-sm mt-2">
                     Didn’t receive the OTP?
-                    <a href="#" class="text-[#ACFC03] font-semibold underline">Resend</a>
+                    <a href="#" className="text-[#ACFC03] font-semibold underline">Resend</a>
                 </p>
             </div>
         </div>
--- movie-projects/client/src/pages/Register.jsx
+++ movie-projects/client/src/pages/Register.jsx
@@ -2,17 +2,20 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import axios from 'axios'
+import OTPVerification from './OTPVerification'
 
 export default function Register() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [showOTPVerify, setShowOTPVerify] = useState(true)
   const postRegisterInfo = (e) => {
     e.preventDefault()
     try {
       // const res = axios.post("http://localhost:4000/auth/register", {name, email, password})
       // console.log(res)
-      toast.success("OTP has been sent to your email. Please check your gmail.", { theme: "dark",autoClose: 3000 })
+      toast.success("OTP has been sent to your email. Please check your gmail.", { theme: "dark", autoClose: 3000 })
+      setShowOTPVerify(!showOTPVerify)
     } catch (error) {
       console.log(error)
       toast.error("Failed to send OTP and try again.", { theme: "dark", autoClose: 3000 })
@@ -20,46 +23,53 @@ export default function Register() {
   }
 
   return (
-    <div className="h-[85vh] flex justify-center items-center bg-gray-800">
-      <div className="w-[400px] bg-gray-900 shadow-lg rounded-lg p-6">
-        <h1 className="text-3xl font-semibold text-center text-white mb-4">Register</h1>
-        <form className="space-y-4" onSubmit={(e) => postRegisterInfo(e)}>
-          <div className="flex flex-col">
-            <label className="font-medium mb-1 text-gray-300">Enter Your Name</label>
-            <input
-              type="text"
-              placeholder="John Doe"
-              className="border border-gray-600 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]"
-              onChange={(e) => setName(e.target.value)}
-            />
+    <div className='h-[85vh] flex justify-center items-center'>
+      {
+        showOTPVerify ? <div className="h-[85vh] flex justify-center items-center bg-gray-800">
+          <div className="w-[400px] bg-gray-900 shadow-lg rounded-lg p-6">
+            <h1 className="text-3xl font-semibold text-center text-white mb-4">Register</h1>
+            <form className="space-y-4" onSubmit={(e) => postRegisterInfo(e)}>
+              <div className="flex flex-col">
+                <label className="font-medium mb-1 text-gray-300">Enter Your Name</label>
+                <input
+                  type="text"
+                  placeholder="John Doe"
+                  className="border border-gray-600 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="font-medium mb-1 text-gray-300">Enter Your Email</label>
+                <input
+                  type="email"
+                  placeholder="example@gmail.com"
+                  className="border border-gray-600 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-col">
+                <label className="font-medium mb-1 text-gray-300">Enter Your Password</label>
+                <input
+                  type="password"
+                  placeholder="********"
+                  className="border border-gray-600 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-[#ACFC03] text-black font-semibold py-2 mt-4 rounded-md hover:bg-[#adfc03e1] transition cursor-pointer"
+              >
+                Register
+              </button>
+              <p className='text-center text-[15px] text-gray-400'>Already have an account? <Link to={'/signin'} className='text-[#ACFC03] font-semibold underline'>Login</Link></p>
+            </form>
           </div>
-          <div className="flex flex-col">
-            <label className="font-medium mb-1 text-gray-300">Enter Your Email</label>
-            <input
-              type="email"
-              placeholder="example@gmail.com"
-              className="border border-gray-600 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]"
-              onChange={(e) => setEmail(e.target.value)}
-            />
+        </div> :
+          <div className="h-full w-full flex justify-center items-center bg-gray-700/50 backdrop-blur-sm absolute top-0">
+            <OTPVerification />
           </div>
-          <div className="flex flex-col">
-            <label className="font-medium mb-1 text-gray-300">Enter Your Password</label>
-            <input
-              type="password"
-              placeholder="********"
-              className="border border-gray-600 bg-gray-800 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#adfc03bb]"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-[#ACFC03] text-black font-semibold py-2 mt-4 rounded-md hover:bg-[#adfc03e1] transition cursor-pointer"
-          >
-            Register
-          </button>
-          <p className='text-center text-[15px] text-gray-400'>Already have an account? <Link to={'/signin'} className='text-[#ACFC03] font-semibold underline'>Login</Link></p>
-        </form>
-      </div>
+      }
     </div>
   )
 }
